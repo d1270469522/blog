6a3afb52b84fdf814c07e3f892ba7395e5e739d2
董鹏飞 <1270469522@qq.com>
2020-09-03 08:54:02 +0000

    博客列表的html 格式化

--- resources/views/shard/_sidebar.blade.php
+++ resources/views/shard/_sidebar.blade.php
@@ -1,81 +1,99 @@
 <div class="col-other">
-    <div class="inner">
-        <div class="other-item" id="categoryandsearch">
-            <div class="search">
-                <label class="search-wrap">
-                    <input type="text" id="searchtxt" placeholder="输入关键字搜索" />
-                    <span class="search-icon">
-                        <i class="fa fa-search"></i>
-                    </span>
-                </label>
-                <ul class="search-result"></ul>
-            </div>
-            <ul class="category mt20" id="category">
-                <li data-index="0" class="slider"></li>
-                <li data-index="1"><a href="/Blog/Article">全部文章</a></li>
-                <li data-index="2"><a href="/Blog/Article/1/">Java</a></li>
-                <li data-index="3"><a href="/Blog/Article/2/">前端</a></li>
-                <li data-index="4"><a href="/Blog/Article/3/">Python</a></li>
-                <li data-index="5"><a href="/Blog/Article/4/">PHP</a></li>
-                <li data-index="6"><a href="/Blog/Article/5/">其它</a></li>
-            </ul>
-        </div>
-        <!--右边悬浮 平板或手机设备显示-->
-        <div class="category-toggle"><i class="layui-icon">&#xe603;</i></div>
-        <div class="article-category">
-            <div class="article-category-title">分类导航</div>
-                    <a href="/Blog/Article/1/">Java</a>
-                    <a href="/Blog/Article/2/">前端</a>
-                    <a href="/Blog/Article/3/">Python</a>
-                    <a href="/Blog/Article/4/">PHP</a>
-                    <a href="/Blog/Article/5/">其它</a>
-            <div class="f-cb"></div>
-        </div>
-        <!--遮罩-->
-        <div class="blog-mask animated layui-hide"></div>
-        <div class="other-item">
-            <h5 class="other-item-title">热门文章</h5>
-            <div class="inner">
-                <ul class="hot-list-article">
-                        <li> <a href="/Blog/Read/9">SpringBoot 入门爬虫项目实战</a></li>
-                        <li> <a href="/Blog/Read/12">SpringBoot 2.x 教你快速入门</a></li>
-                        <li> <a href="/Blog/Read/13">java学习路线</a></li>
-                        <li> <a href="/Blog/Read/4">基于SpringBoot+JWT+Redis跨域单点登录的实现</a></li>
-                        <li> <a href="/Blog/Read/7">SpringBoot 中如何使用SwaggerAPI接口文档？</a></li>
-                        <li> <a href="/Blog/Read/11">小白轻松入门Redis</a></li>
-                        <li> <a href="/Blog/Read/14">微信一键登录功能的实现</a></li>
-                        <li> <a href="/Blog/Read/8">NPOI导入导出Excel</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="other-item">
-            <h5 class="other-item-title">置顶推荐</h5>
-            <div class="inner">
-                <ul class="hot-list-article">
-                        <li> <a href="/Blog/Read/16">java学习路线</a></li>
-                        <li> <a href="/Blog/Read/14">基于SpringBoot+JWT+Redis跨域单点登录的实现</a></li>
-                        <li> <a href="/Blog/Read/9">小白轻松入门Redis</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="other-item">
-            <h5 class="other-item-title">最近访客</h5>
-            <div class="inner">
-                <dl class="vistor">
-                        <dd><a href="javasript:;"><img src="https://thirdqq.qlogo.cn/qqapp/101465933/72388EA977643E8F97111222675720B1/100"><cite>Anonymous</cite></a></dd>
-                        <dd><a href="javasript:;"><img src="https://thirdqq.qlogo.cn/qqapp/101465933/342F777E313DDF5CCD6E3E707BB0770B/100"><cite>Dekstra</cite></a></dd>
-                        <dd><a href="javasript:;"><img src="https://thirdqq.qlogo.cn/qqapp/101465933/EA5D00A72C0C43ECD8FC481BD274DEEC/100"><cite>惜i</cite></a></dd>
-                        <dd><a href="javasript:;"><img src="https://thirdqq.qlogo.cn/qqapp/101465933/EF18CEC98150D2442183AA30F05AAD7B/100"><cite>↙Aㄨ计划 ◆莪↘</cite></a></dd>
-                        <dd><a href="javasript:;"><img src="https://thirdqq.qlogo.cn/qqapp/101465933/3D8D91AD2BAFD36F5AC494DA51E270E6/100"><cite>.</cite></a></dd>
-                        <dd><a href="javasript:;"><img src="https://thirdqq.qlogo.cn/qqapp/101465933/B745A110DAB712A0E6C5D0B633E905D3/100"><cite>Lambert.</cite></a></dd>
-                        <dd><a href="javasript:;"><img src="https://thirdqq.qlogo.cn/qqapp/101465933/E9BA3A2499EC068B7917B9EF45C4D13C/100"><cite>64ღ</cite></a></dd>
-                        <dd><a href="javasript:;"><img src="https://thirdqq.qlogo.cn/qqapp/101465933/09F92966169272DD7DD9999E709A0204/100"><cite>doBoor</cite></a></dd>
-                        <dd><a href="javasript:;"><img src="https://thirdqq.qlogo.cn/qqapp/101465933/59991D53192643A1A651383847332EB6/100"><cite>毛毛小妖</cite></a></dd>
-                        <dd><a href="javasript:;"><img src="https://thirdqq.qlogo.cn/qqapp/101465933/FF34F311DDC43E2AF63BE897BCA24F05/100"><cite>NULL</cite></a></dd>
-                        <dd><a href="javasript:;"><img src="https://thirdqq.qlogo.cn/qqapp/101465933/59AA25A7627284AE62C8E6EBDC6FE417/100"><cite>吓一跳</cite></a></dd>
-                        <dd><a href="javasript:;"><img src="https://thirdqq.qlogo.cn/qqapp/101465933/28B021E0F5AF0A4B9B781A24329FE897/100"><cite>如初</cite></a></dd>
-                </dl>
-            </div>
-        </div>
+  <div class="inner">
+
+
+    <!-- 分类导航 -->
+    <div class="other-item" id="categoryandsearch">
+      <div class="search">
+        <label class="search-wrap">
+          <input type="text" id="searchtxt" placeholder="输入关键字搜索" />
+          <span class="search-icon">
+            <i class="fa fa-search"></i>
+          </span>
+        </label>
+        <ul class="search-result"></ul>
+      </div>
+      <ul class="category mt20" id="category">
+        <li data-index="0" class="slider"></li>
+        <li data-index="1"><a href="{{ route('topics.index') }}">全部文章</a></li>
+        <li data-index="2"><a href="{{ route('topics.index') }}">Java</a></li>
+        <li data-index="3"><a href="{{ route('topics.index') }}">前端</a></li>
+        <li data-index="3"><a href="{{ route('topics.index') }}">前端</a></li>
+        <li data-index="3"><a href="{{ route('topics.index') }}">前端</a></li>
+        <li data-index="3"><a href="{{ route('topics.index') }}">前端</a></li>
+      </ul>
     </div>
+
+    <!--右边悬浮 平板或手机设备显示-->
+    <div class="category-toggle"><i class="layui-icon">&#xe603;</i></div>
+    <div class="article-category">
+      <div class="article-category-title">分类导航</div>
+        <a href="{{ route('topics.index') }}">Java</a>
+        <a href="{{ route('topics.index') }}">前端</a>
+      <div class="f-cb"></div>
+    </div>
+
+    <!--热门文章-->
+    <div class="blog-mask animated layui-hide"></div>
+    <div class="other-item">
+      <h5 class="other-item-title">热门文章</h5>
+      <div class="inner">
+        <ul class="hot-list-article">
+          <li> <a href="{{ route('topics.index') }}">SpringBoot 入门爬虫项目实战</a></li>
+          <li> <a href="{{ route('topics.index') }}">SpringBoot 2.x 教你快速入门</a></li>
+          <li> <a href="{{ route('topics.index') }}">java学习路线</a></li>
+          <li> <a href="{{ route('topics.index') }}">基于SpringBoot+JWT+Redis跨域单点登录的实现</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <!--热门文章-->
+    <div class="blog-mask animated layui-hide"></div>
+    <div class="other-item">
+      <h5 class="other-item-title">热门文章</h5>
+      <div class="inner">
+        <ul class="hot-list-article">
+          <li> <a href="{{ route('topics.index') }}">SpringBoot 入门爬虫项目实战</a></li>
+          <li> <a href="{{ route('topics.index') }}">SpringBoot 2.x 教你快速入门</a></li>
+          <li> <a href="{{ route('topics.index') }}">java学习路线</a></li>
+          <li> <a href="{{ route('topics.index') }}">基于SpringBoot+JWT+Redis跨域单点登录的实现</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <!--热门文章-->
+    <div class="blog-mask animated layui-hide"></div>
+    <div class="other-item">
+      <h5 class="other-item-title">热门文章</h5>
+      <div class="inner">
+        <ul class="hot-list-article">
+          <li> <a href="{{ route('topics.index') }}">SpringBoot 入门爬虫项目实战</a></li>
+          <li> <a href="{{ route('topics.index') }}">SpringBoot 2.x 教你快速入门</a></li>
+          <li> <a href="{{ route('topics.index') }}">java学习路线</a></li>
+          <li> <a href="{{ route('topics.index') }}">基于SpringBoot+JWT+Redis跨域单点登录的实现</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <!-- 最近访客 -->
+    <div class="other-item">
+      <h5 class="other-item-title">最近访客</h5>
+      <div class="inner">
+        <dl class="vistor">
+          <dd><a href="{{ route('topics.index') }}">
+            <img src="{{ URL::asset('images/wu.jpg') }}">
+            <cite>Anonymous</cite>
+          </a></dd>
+          <dd><a href="{{ route('topics.index') }}">
+            <img src="{{ URL::asset('images/wu.jpg') }}">
+            <cite>Dekstra</cite>
+          </a></dd>
+          <dd><a href="{{ route('topics.index') }}">
+            <img src="{{ URL::asset('images/wu.jpg') }}">
+            <cite>惜i</cite>
+          </a></dd>
+        </dl>
+      </div>
+    </div>
+  </div>
 </div>
