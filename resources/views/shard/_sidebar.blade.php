5a7f8fb0eab43d31ef2231fb059b072ad5e5bf04
董鹏飞 <1270469522@qq.com>
2020-09-08 14:27:02 +0000

    首页 ｜ 列表 处理完成

--- resources/views/shard/_sidebar.blade.php
+++ resources/views/shard/_sidebar.blade.php
@@ -15,9 +15,9 @@
       <ul class="category mt20" id="category">
         <li data-index="0" class="slider"></li>
         <li data-index="1"><a href="{{ route('topics.index') }}">全部文章</a></li>
-        <li data-index="2"><a href="{{ route('topics.index') }}">Java</a></li>
-        <li data-index="3"><a href="{{ route('topics.index') }}">前端</a></li>
-        <li data-index="4"><a href="{{ route('topics.index') }}">前端</a></li>
+        <li data-index="2"><a href="{{ route('topics.index') }}">分类一</a></li>
+        <li data-index="3"><a href="{{ route('topics.index') }}">分类二</a></li>
+        <li data-index="4"><a href="{{ route('topics.index') }}">分类三</a></li>
       </ul>
     </div>
 
@@ -25,8 +25,9 @@
     <div class="category-toggle"><i class="layui-icon">&#xe603;</i></div>
     <div class="article-category">
       <div class="article-category-title">分类导航</div>
-        <a href="{{ route('topics.index') }}">Java</a>
-        <a href="{{ route('topics.index') }}">前端</a>
+        <a href="{{ route('topics.index') }}">分类一</a>
+        <a href="{{ route('topics.index') }}">分类二</a>
+        <a href="{{ route('topics.index') }}">分类三</a>
       <div class="f-cb"></div>
     </div>
 
@@ -36,10 +37,10 @@
       <h5 class="other-item-title">热门文章</h5>
       <div class="inner">
         <ul class="hot-list-article">
-          <li> <a href="{{ route('topics.index') }}">SpringBoot 入门爬虫项目实战</a></li>
-          <li> <a href="{{ route('topics.index') }}">SpringBoot 2.x 教你快速入门</a></li>
-          <li> <a href="{{ route('topics.index') }}">java学习路线</a></li>
-          <li> <a href="{{ route('topics.index') }}">基于SpringBoot+JWT+Redis跨域单点登录的实现</a></li>
+          <li> <a href="{{ route('topics.index') }}">热门一</a></li>
+          <li> <a href="{{ route('topics.index') }}">热门二</a></li>
+          <li> <a href="{{ route('topics.index') }}">热门三</a></li>
+          <li> <a href="{{ route('topics.index') }}">热门四</a></li>
         </ul>
       </div>
     </div>
@@ -50,10 +51,10 @@
       <h5 class="other-item-title">热门文章</h5>
       <div class="inner">
         <ul class="hot-list-article">
-          <li> <a href="{{ route('topics.index') }}">SpringBoot 入门爬虫项目实战</a></li>
-          <li> <a href="{{ route('topics.index') }}">SpringBoot 2.x 教你快速入门</a></li>
-          <li> <a href="{{ route('topics.index') }}">java学习路线</a></li>
-          <li> <a href="{{ route('topics.index') }}">基于SpringBoot+JWT+Redis跨域单点登录的实现</a></li>
+          <li> <a href="{{ route('topics.index') }}">热门一</a></li>
+          <li> <a href="{{ route('topics.index') }}">热门二</a></li>
+          <li> <a href="{{ route('topics.index') }}">热门三</a></li>
+          <li> <a href="{{ route('topics.index') }}">热门四</a></li>
         </ul>
       </div>
     </div>
@@ -64,10 +65,10 @@
       <h5 class="other-item-title">热门文章</h5>
       <div class="inner">
         <ul class="hot-list-article">
-          <li> <a href="{{ route('topics.index') }}">SpringBoot 入门爬虫项目实战</a></li>
-          <li> <a href="{{ route('topics.index') }}">SpringBoot 2.x 教你快速入门</a></li>
-          <li> <a href="{{ route('topics.index') }}">java学习路线</a></li>
-          <li> <a href="{{ route('topics.index') }}">基于SpringBoot+JWT+Redis跨域单点登录的实现</a></li>
+          <li> <a href="{{ route('topics.index') }}">热门一</a></li>
+          <li> <a href="{{ route('topics.index') }}">热门二</a></li>
+          <li> <a href="{{ route('topics.index') }}">热门三</a></li>
+          <li> <a href="{{ route('topics.index') }}">热门四</a></li>
         </ul>
       </div>
     </div>
@@ -79,15 +80,15 @@
         <dl class="vistor">
           <dd><a href="{{ route('topics.index') }}">
             <img src="{{ URL::asset('images/wu.jpg') }}">
-            <cite>Anonymous</cite>
+            <cite>访客一</cite>
           </a></dd>
           <dd><a href="{{ route('topics.index') }}">
             <img src="{{ URL::asset('images/wu.jpg') }}">
-            <cite>Dekstra</cite>
+            <cite>访客二</cite>
           </a></dd>
           <dd><a href="{{ route('topics.index') }}">
             <img src="{{ URL::asset('images/wu.jpg') }}">
-            <cite>惜i</cite>
+            <cite>访客三</cite>
           </a></dd>
         </dl>
       </div>
