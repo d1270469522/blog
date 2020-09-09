e9439438262c168bb28c82b8acaf603941610239
董鹏飞 <1270469522@qq.com>
2020-09-09 14:59:39 +0000

    首页 js 调整

--- public/js/index.js
+++ public/js/index.js
@@ -2,33 +2,36 @@ layui.use(['jquery', 'util'], function () {
   var $    = layui.jquery;
   var util = layui.util;
 
+  // 初始化 wow.js
   $(window).load(function () {
     new WOW().init();
   })
 
-  // 一键至顶
+  // 一键置顶
   util.fixbar();
 
-  $('.next').click(function () {
+  // 首页：第一模块 点击向下箭头跳转第二模块
+  $('#next').click(function () {
     $('html,body').animate({
       scrollTop: $('#section1').outerHeight() - 40
     }, 600);
   });
 
+  // 右上角菜单按钮 开关
   $('#menu').on('click', function () {
     var mark = $(this).attr('data-mark');
     if (mark === 'false') {
       $(this).removeClass('menu_open').addClass('menu_close');
-      //open
       $('#navgation').removeClass('navgation_close').addClass('navgation_open');
       $(this).attr({ 'data-mark': "true" });
     } else {
       $(this).removeClass('menu_close').addClass('menu_open');
-      //close
       $('#navgation').removeClass('navgation_open').addClass('navgation_close');
       $(this).attr({ 'data-mark': "false" });
     }
   });
+
+  // 右上角菜单按钮开了以后，点击页面遮罩层都可以关闭
   $('#navgation').on('click', function () {
     var mark = $('#menu').attr('data-mark');
     if (mark === 'true') {
